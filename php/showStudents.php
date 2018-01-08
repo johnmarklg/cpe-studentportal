90f184e0f68d6a9bf3ce06e9ea4798a3ddbf024d
John Mark Leaño <markz6197@users.noreply.github.com>
2018-01-08 20:31:05 +0800

    some things left
    delete rows on student list and events/holidays
    file upload
    papers/pdf generation
    hymn/march/oaths

--- php/showStudents.php
+++ php/showStudents.php
@@ -82,7 +82,7 @@
 		<tbody class=\"list\">";
 
 		$conn = getDB('cpe-studentportal');
-		$stmt = $conn->prepare("SELECT * from students WHERE yearstarted = :fifthyear");
+		$stmt = $conn->prepare("SELECT * from students WHERE yearstarted <= :fifthyear AND studnum <> 00-0000");
 		$stmt -> bindParam(':fifthyear', $fifthyear);
 		$stmt->execute();
 
@@ -103,5 +103,181 @@
 		}
 		$conn = null;
 		echo "</tbody></table></div></div></div></div></div>";	
+		
+		//4TH YEAR
+		$fourthyear = $fifthyear + 1;
+		echo "<div class=\"row\"><div class=\"col-lg-12\"><div class=\"panel panel-default\">
+		<div class=\"panel-heading\">Fourth Year (= ". $fourthyear . ")</div><div class=\"panel-body\"><div class=\"table-responsive\">
+		<table id=\"tablefifth\" class=\"table\">
+		<thead>
+		<tr>
+			<th style=\"font-size: 0px\">Old Student Number</th>
+			<th style=\"font-size: 0px\">ID</th>
+			<th>Student Number</th>
+			<th>Surname</th>
+			<th>First Name</th>
+			<th>Middle Name</th>
+			<th>CFAT Score</th>
+			<th>Passcode</th>
+			<th style=\"font-size: 0px\"></th>
+			<th style=\"font-size: 0px\">Year Started</th>
+		</tr>
+		</thead>
+		<tbody class=\"list\">";
+
+		$conn = getDB('cpe-studentportal');
+		$stmt = $conn->prepare("SELECT * from students WHERE yearstarted = :fourthyear");
+		$stmt -> bindParam(':fourthyear', $fourthyear);
+		$stmt->execute();
+
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+		foreach(($stmt->fetchAll()) as $row) { 
+			echo "<tr>
+			<td style=\"font-size: 0px\">" . $row['studnum'] . "</td>
+			<td style=\"font-size: 0px\" class=\"id\">" . $row['id'] . "</td>
+			<td>" . $row['studnum'] . "</td>
+			<td>" . $row['surname'] . "</td>
+			<td>" . $row['firstname'] . "</td>
+			<td>" . $row['middlename'] . "</td>
+			<td>" . $row['cfatscore'] . "</td>
+			<td>" . $row['passcode'] . "</td>
+			<td><i style=\"vertical-align: bottom;\" class=\"table-remove material-icons\">remove_circle_outline</i></td>
+			<td style=\"font-size: 0px\" class=\"yearstarted\">" . $row['yearstarted'] . "</td></tr>";
+		}
+		$conn = null;
+		echo "</tbody></table></div></div></div></div></div>";	
+		
+		//3RD YEAR
+		$thirdyear = $fifthyear + 2;
+		echo "<div class=\"row\"><div class=\"col-lg-12\"><div class=\"panel panel-default\">
+		<div class=\"panel-heading\">Third Year (= ". $thirdyear . ")</div><div class=\"panel-body\"><div class=\"table-responsive\">
+		<table id=\"tablefifth\" class=\"table\">
+		<thead>
+		<tr>
+			<th style=\"font-size: 0px\">Old Student Number</th>
+			<th style=\"font-size: 0px\">ID</th>
+			<th>Student Number</th>
+			<th>Surname</th>
+			<th>First Name</th>
+			<th>Middle Name</th>
+			<th>CFAT Score</th>
+			<th>Passcode</th>
+			<th style=\"font-size: 0px\"></th>
+			<th style=\"font-size: 0px\">Year Started</th>
+		</tr>
+		</thead>
+		<tbody class=\"list\">";
+
+		$conn = getDB('cpe-studentportal');
+		$stmt = $conn->prepare("SELECT * from students WHERE yearstarted = :thirdyear");
+		$stmt -> bindParam(':thirdyear', $thirdyear);
+		$stmt->execute();
+
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+		foreach(($stmt->fetchAll()) as $row) { 
+			echo "<tr>
+			<td style=\"font-size: 0px\">" . $row['studnum'] . "</td>
+			<td style=\"font-size: 0px\" class=\"id\">" . $row['id'] . "</td>
+			<td>" . $row['studnum'] . "</td>
+			<td>" . $row['surname'] . "</td>
+			<td>" . $row['firstname'] . "</td>
+			<td>" . $row['middlename'] . "</td>
+			<td>" . $row['cfatscore'] . "</td>
+			<td>" . $row['passcode'] . "</td>
+			<td><i style=\"vertical-align: bottom;\" class=\"table-remove material-icons\">remove_circle_outline</i></td>
+			<td style=\"font-size: 0px\" class=\"yearstarted\">" . $row['yearstarted'] . "</td></tr>";
+		}
+		$conn = null;
+		echo "</tbody></table></div></div></div></div></div>";	
+		
+		//2ND YEAR
+		$secondyear= $fifthyear + 3;
+		echo "<div class=\"row\"><div class=\"col-lg-12\"><div class=\"panel panel-default\">
+		<div class=\"panel-heading\">Second Year (= ". $secondyear . ")</div><div class=\"panel-body\"><div class=\"table-responsive\">
+		<table id=\"tablefifth\" class=\"table\">
+		<thead>
+		<tr>
+			<th style=\"font-size: 0px\">Old Student Number</th>
+			<th style=\"font-size: 0px\">ID</th>
+			<th>Student Number</th>
+			<th>Surname</th>
+			<th>First Name</th>
+			<th>Middle Name</th>
+			<th>CFAT Score</th>
+			<th>Passcode</th>
+			<th style=\"font-size: 0px\"></th>
+			<th style=\"font-size: 0px\">Year Started</th>
+		</tr>
+		</thead>
+		<tbody class=\"list\">";
+
+		$conn = getDB('cpe-studentportal');
+		$stmt = $conn->prepare("SELECT * from students WHERE yearstarted = :secondyear");
+		$stmt -> bindParam(':secondyear', $secondyear);
+		$stmt->execute();
+
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+		foreach(($stmt->fetchAll()) as $row) { 
+			echo "<tr>
+			<td style=\"font-size: 0px\">" . $row['studnum'] . "</td>
+			<td style=\"font-size: 0px\" class=\"id\">" . $row['id'] . "</td>
+			<td>" . $row['studnum'] . "</td>
+			<td>" . $row['surname'] . "</td>
+			<td>" . $row['firstname'] . "</td>
+			<td>" . $row['middlename'] . "</td>
+			<td>" . $row['cfatscore'] . "</td>
+			<td>" . $row['passcode'] . "</td>
+			<td><i style=\"vertical-align: bottom;\" class=\"table-remove material-icons\">remove_circle_outline</i></td>
+			<td style=\"font-size: 0px\" class=\"yearstarted\">" . $row['yearstarted'] . "</td></tr>";
+		}
+		$conn = null;
+		echo "</tbody></table></div></div></div></div></div>";
+		
+		//1ST YEAR
+		$firstyear = $fifthyear + 4;
+		echo "<div class=\"row\"><div class=\"col-lg-12\"><div class=\"panel panel-default\">
+		<div class=\"panel-heading\">First Year (= ". $firstyear . ")</div><div class=\"panel-body\"><div class=\"table-responsive\">
+		<table id=\"tablefifth\" class=\"table\">
+		<thead>
+		<tr>
+			<th style=\"font-size: 0px\">Old Student Number</th>
+			<th style=\"font-size: 0px\">ID</th>
+			<th>Student Number</th>
+			<th>Surname</th>
+			<th>First Name</th>
+			<th>Middle Name</th>
+			<th>CFAT Score</th>
+			<th>Passcode</th>
+			<th style=\"font-size: 0px\"></th>
+			<th style=\"font-size: 0px\">Year Started</th>
+		</tr>
+		</thead>
+		<tbody class=\"list\">";
+
+		$conn = getDB('cpe-studentportal');
+		$stmt = $conn->prepare("SELECT * from students WHERE yearstarted = :firstyear");
+		$stmt -> bindParam(':firstyear', $firstyear);
+		$stmt->execute();
+
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+		foreach(($stmt->fetchAll()) as $row) { 
+			echo "<tr>
+			<td style=\"font-size: 0px\">" . $row['studnum'] . "</td>
+			<td style=\"font-size: 0px\" class=\"id\">" . $row['id'] . "</td>
+			<td>" . $row['studnum'] . "</td>
+			<td>" . $row['surname'] . "</td>
+			<td>" . $row['firstname'] . "</td>
+			<td>" . $row['middlename'] . "</td>
+			<td>" . $row['cfatscore'] . "</td>
+			<td>" . $row['passcode'] . "</td>
+			<td><i style=\"vertical-align: bottom;\" class=\"table-remove material-icons\">remove_circle_outline</i></td>
+			<td style=\"font-size: 0px\" class=\"yearstarted\">" . $row['yearstarted'] . "</td></tr>";
+		}
+		$conn = null;
+		echo "</tbody></table></div></div></div></div></div>";
 	}
 ?>
