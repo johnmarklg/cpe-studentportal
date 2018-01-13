1578810bcd82012552733f3624fb51659058254c
John Mark Leaño <markz6197@users.noreply.github.com>
2018-01-13 10:56:37 +0800

    calendar's good, next prospectus
    it still looks ugly but design can be done later

--- events.php
+++ events.php
@@ -9,7 +9,7 @@ try {
     exit('Unable to connect to database.');
 }
 $pdo = Database::connect();
-$res = "SELECT * FROM events";
+$res = "SELECT * FROM (SELECT * FROM events UNION ALL SELECT * FROM holidays) subquery ORDER BY FIELD(start, end)";
 $events = $pdo->query($res) or die(print_r($pdo->errorInfo()));
 
 echo json_encode($events->fetchAll(PDO::FETCH_ASSOC));
