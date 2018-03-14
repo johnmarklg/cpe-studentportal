528cd822735e0b2bf6f8d0d6ac7c499571267450
John Mark Leaño <markz6197@users.noreply.github.com>
2018-03-14 21:06:28 +0800

    categorized announcements for bulletin

--- admin/index.php
+++ admin/index.php
@@ -94,57 +94,140 @@ $conn = getDB('cpe-studentportal');
 				
 				<div class="row">
 					<div class="col-lg-12">
-						<div class="panel panel-primary">
-							<div class="panel-heading">
-							Shown Announcements
-							</div><div class="panel-body">
-								<?php
-									if($_SESSION['name'][0]=='Administrator (Elevated)') {$stmt = $conn->prepare("SELECT posts.*, administrators.name as poster from `posts` 
-									LEFT JOIN administrators
-									ON administrators.id = posts.posterid
-									WHERE `status` = 'Approved' ORDER BY datetime DESC"); } else {
-									$stmt = $conn->prepare("SELECT posts.*, administrators.name as poster from `posts` 
-									LEFT JOIN administrators
-									ON administrators.id = posts.posterid
-									WHERE `status` = 'Approved' AND showbulletin != 2 ORDER BY datetime DESC");	
-									}
-									$stmt->execute();
-										//approved already
-										foreach(($stmt->fetchAll()) as $row) { 
-											$time = strtotime($row['datetime']);
-											echo '<div class="panel panel-default">';
-											echo '<div class="panel-heading">' . '<a data-toggle="collapse" href="#collapsePanel' . $row['id'] . '"><strong>' . $row['poster'] . '</strong></a> @ <i>' . relativeTime($time) . '</i>';
-											if (($row['filetype'] == 'gif')||($row['filetype'] == 'jpg')||($row['filetype'] == 'png')||($row['filetype'] == 'webp')) {
-												echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-2">' .
-												'<a href="/uploads/' . $row['file'] . '" class="swipebox" title="' . $row['posttitle'] . '"><img style="max-height: 25vh; max-width: 100%; border:1px solid #021a40" src="/uploads/' . $row['file'] . '"></a>'
-												. '</div><div class="col-lg-10"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '</div>';
-											} else if ($row['file'] == ''){
-												echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-12"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '</div>';
-											} else {
-												echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-12"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '<hr/>
-												<a href="/uploads/' . $row['file'] . '" title="' . $row['posttitle'] . '">Click here to view/download attachment</a></div>';
-											}
-											echo '</div></div>';
-											echo '<div class="panel-footer">';
-											echo '<select id="' . $row['id'] .'" name="'. $row['datetime'] .'" class="showhide form-control" aria-label="close" style="font-size: 2.5vh; color: #4f4f4f" onclick="showhide_cache=this.value;">';
-											if ($row['showbulletin']=='1') { 
-												echo '<option value="1" selected>Shown</option> 
-												<option value="0">Hide</option>
-												<option value="2">Dismiss</option>'; 
-											} else if ($row['showbulletin']=='0') {
-												echo '<option value="1">Show</option> 
-												<option value="0" selected>Hidden</option>
-												<option value="2">Dismiss</option>';
-											} else {
-												echo '<option value="1">Undismiss, Show</option> 
-												<option value="0">Undismiss, Hide</option>
-												<option value="2" selected>Dismissed</option>';
-											}
-											echo '</select>';
-											echo '</div></div>';
-										}				
-								?>
-									</div>
+						<div class="panel panel-info">
+							<div class="panel-heading" style="text-align: center;" id="exTab2">	
+								<ul class="nav nav-pills nav-justified">
+									<li class="active">
+									<a  id="1a" href="#1b" data-toggle="tab">For Carousel</a>
+									</li>
+									<li><a id="2a" href="#2b" data-toggle="tab">For Ticker</a>
+									</li>
+									<li><a id="3a" href="#3b" data-toggle="tab">Dismissed</a>
+									</li>
+								</ul>
+							</div>
+
+							<div class="panel-body tab-content ">
+								<div class="tab-pane active" id="1b">
+									<?php
+										$stmt = $conn->prepare("SELECT posts.*, administrators.name as poster from `posts` 
+										LEFT JOIN administrators
+										ON administrators.id = posts.posterid
+										WHERE `status` = 'Approved' AND showbulletin != 2 AND (filetype = 'gif' OR filetype = 'jpg' OR filetype = 'png' OR filetype = 'webp') ORDER BY datetime DESC");	
+										$stmt->execute();
+											//approved already
+											foreach(($stmt->fetchAll()) as $row) { 
+												$time = strtotime($row['datetime']);
+												echo '<div class="panel panel-default"><div class="panel-heading">' . 
+												'<a data-toggle="collapse" href="#collapsePanel' . $row['id'] . '"><strong>' . $row['poster'] . '</strong></a> @ <i>' . relativeTime($time) . '</i>';
+													if (($row['filetype'] == 'gif')||($row['filetype'] == 'jpg')||($row['filetype'] == 'png')||($row['filetype'] == 'webp')) {
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-2">' .
+														'<a href="/uploads/' . $row['file'] . '" class="swipebox" title="' . $row['posttitle'] . '"><img style="max-height: 25vh; max-width: 100%; border:1px solid #021a40" src="/uploads/' . $row['file'] . '"></a>'
+														. '</div><div class="col-lg-10"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '</div>';
+													} else if ($row['file'] == ''){
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-12"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '</div>';
+													} else {
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-12"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '<hr/>
+														<a href="/uploads/' . $row['file'] . '" title="' . $row['posttitle'] . '">Click here to view/download attachment</a></div>';
+													}
+												echo '</div></div>';
+												echo '<div class="panel-footer">';
+												echo '<select id="' . $row['id'] .'" name="'. $row['datetime'] .'" class="showhide form-control" aria-label="close" style="font-size: 2.5vh; color: #4f4f4f" onclick="showhide_cache=this.value;">';
+												if ($row['showbulletin']=='1') { 
+													echo '<option value="1" selected>Shown</option> 
+													<option value="0">Hide</option>
+													<option value="2">Dismiss</option>'; 
+												} else if ($row['showbulletin']=='0') {
+													echo '<option value="1">Show</option> 
+													<option value="0" selected>Hidden</option>
+													<option value="2">Dismiss</option>';
+												}
+												echo '</select>';
+												echo '</div></div>';
+											}				
+									?>
+								</div>
+								<div class="tab-pane" id="2b">
+									<?php
+										$stmt = $conn->prepare("SELECT posts.*, administrators.name as poster from `posts` 
+										LEFT JOIN administrators
+										ON administrators.id = posts.posterid
+										WHERE `status` = 'Approved' AND showbulletin != 2 AND (filetype != 'gif' AND filetype != 'jpg' AND filetype != 'png' AND filetype != 'webp') ORDER BY datetime DESC");
+										$stmt->execute();
+											//approved already
+											foreach(($stmt->fetchAll()) as $row) { 
+												$time = strtotime($row['datetime']);
+												echo '<div class="panel panel-default"><div class="panel-heading">' . 
+												'<a data-toggle="collapse" href="#collapsePanel' . $row['id'] . '"><strong>' . $row['poster'] . '</strong></a> @ <i>' . relativeTime($time) . '</i>';
+													if (($row['filetype'] == 'gif')||($row['filetype'] == 'jpg')||($row['filetype'] == 'png')||($row['filetype'] == 'webp')) {
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-2">' .
+														'<a href="/uploads/' . $row['file'] . '" class="swipebox" title="' . $row['posttitle'] . '"><img style="max-height: 25vh; max-width: 100%; border:1px solid #021a40" src="/uploads/' . $row['file'] . '"></a>'
+														. '</div><div class="col-lg-10"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '</div>';
+													} else if ($row['file'] == ''){
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-12"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '</div>';
+													} else {
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-12"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '<hr/>
+														<a href="/uploads/' . $row['file'] . '" title="' . $row['posttitle'] . '">Click here to view/download attachment</a></div>';
+													}
+												echo '</div></div>';
+												echo '<div class="panel-footer">';
+												echo '<select id="' . $row['id'] .'" name="'. $row['datetime'] .'" class="showhide form-control" aria-label="close" style="font-size: 2.5vh; color: #4f4f4f" onclick="showhide_cache=this.value;">';
+												if ($row['showbulletin']=='3') { 
+													echo '<option value="3" selected>Shown</option> 
+													<option value="0">Hide</option>
+													<option value="2">Dismiss</option>'; 
+												} else if ($row['showbulletin']=='0') {
+													echo '<option value="3">Show</option> 
+													<option value="0" selected>Hidden</option>
+													<option value="2">Dismiss</option>';
+												}
+												echo '</select>';
+												echo '</div></div>';
+											}				
+									?>
+								</div>
+								<div class="tab-pane" id="3b">
+									<?php
+										$stmt = $conn->prepare("SELECT posts.*, administrators.name as poster from `posts` 
+										LEFT JOIN administrators
+										ON administrators.id = posts.posterid
+										WHERE `status` = 'Approved' AND showbulletin = 2 ORDER BY datetime DESC");
+										$stmt->execute();
+											//approved already
+											foreach(($stmt->fetchAll()) as $row) { 
+												$time = strtotime($row['datetime']);
+												echo '<div class="panel panel-default"><div class="panel-heading">' . 
+												'<a data-toggle="collapse" href="#collapsePanel' . $row['id'] . '"><strong>' . $row['poster'] . '</strong></a> @ <i>' . relativeTime($time) . '</i>';
+													if (($row['filetype'] == 'gif')||($row['filetype'] == 'jpg')||($row['filetype'] == 'png')||($row['filetype'] == 'webp')) {
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-2">' .
+														'<a href="/uploads/' . $row['file'] . '" class="swipebox" title="' . $row['posttitle'] . '"><img style="max-height: 25vh; max-width: 100%; border:1px solid #021a40" src="/uploads/' . $row['file'] . '"></a>'
+														. '</div><div class="col-lg-10"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '</div>';
+													} else if ($row['file'] == ''){
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-12"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '</div>';
+													} else {
+														echo '</div><div id="collapsePanel'.$row['id'].'" class="panel-collapse collapse"><div class="panel-body"><div class="col-lg-12"><strong>' . $row['posttitle'] . '</strong><hr/>' . $row['post'] . '<hr/>
+														<a href="/uploads/' . $row['file'] . '" title="' . $row['posttitle'] . '">Click here to view/download attachment</a></div>';
+													}
+												echo '</div></div>';
+												echo '<div class="panel-footer">';
+												echo '<select id="' . $row['id'] .'" name="'. $row['datetime'] .'" class="showhide form-control" aria-label="close" style="font-size: 2.5vh; color: #4f4f4f" onclick="showhide_cache=this.value;">';
+												if ($row['showbulletin']=='1') { 
+													echo '<option value="1" selected>Shown</option> 
+													<option value="0">Hide</option>
+													<option value="2">Dismiss</option>'; 
+												} else if ($row['showbulletin']=='0') {
+													echo '<option value="1">Show</option> 
+													<option value="0" selected>Hidden</option>
+													<option value="2">Dismiss</option>';
+												} else {
+													echo '<option value="1">Undismiss, Show</option> 
+													<option value="0">Undismiss, Hide</option>
+													<option value="2" selected>Dismissed</option>';
+												}
+												echo '</select>';
+												echo '</div></div>';
+											}				
+									?>
 								</div>
 							</div>
 						</div>
